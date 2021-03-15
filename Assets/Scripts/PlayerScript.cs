a53133bc4221cdd853774a54e4f7c32a7aecca16
unknown <knight@ucf.edu>
2021-03-14 23:37:08 -0400

    Challenge commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,28 +7,70 @@ public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D rd2d;
     public float speed;
-    public Text score;
-    private int scoreValue = 0;
+    public Text winText;
+    public Text livesText;
+    public Text countText;
+    public int lives;
+    private int count = 0;
+    public AudioSource musicSource;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+    Animator anim;
+    private bool facingRight = true;
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        countText.text = count.ToString();
+        count = 0;
+        lives = 3;
+        SetcountText();
+        winText.text = "";
+        musicSource.clip = musicClipOne;
+        musicSource.Play();
+        musicSource.loop = true;
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float hozMovement = Input.GetAxis("Horizontal");
-        float vertMovement = Input.GetAxis("Vertical");
-        rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
 
         if (Input.GetKeyDown("escape"))
         {
             Application.Quit();
         }
 
+        if (count < 8)
+        {
+            musicSource.clip = musicClipOne;
+        }
+
+        float hozMovement = Input.GetAxis("Horizontal");
+        float vertMovement = Input.GetAxis("Vertical");
+        rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+
+        if (facingRight == false && hozMovement > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
+        }
+
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -36,8 +78,14 @@ public class PlayerScript : MonoBehaviour
         if (collision.collider.tag == "Coin")
         {
             Destroy(collision.collider.gameObject);
-            scoreValue += 1;
-            score.text = scoreValue.ToString();
+            count = count + 1;
+            SetcountText();
+        }
+        else if (collision.collider.tag == "Enemy")
+        {
+            Destroy(collision.collider.gameObject);
+            lives = lives - 1;
+            SetlivesText();
         }
 
     }
@@ -50,6 +98,33 @@ public class PlayerScript : MonoBehaviour
             {
                 rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
             }
+        }
+    }
+
+    void SetcountText()
+    {
+        countText.text = "Score: " + count.ToString();
+        if (count == 4)
+        {
+            transform.position = new Vector2(-65.0f, 1.0f);
+            lives = 3;
+        }
+
+        if (count == 8)
+        {
+            winText.text = "You Win! Game created by Richard";
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+        }
+    }
+
+    void SetlivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+        if (lives == 0)
+        {
+            winText.text = "You Lose!";
+            Destroy(this);
         }
     }
 }
